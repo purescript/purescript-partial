ddbddcd0fcef48c5a6e36363aa3abbc5e6f299a5
Gary Burgess <gary.burgess@gmail.com>
2016-05-20 13:43:16 +0100

    Update build, fix tests for 0.9

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,14 @@
 module Test.Main where
 
+import Partial (crashWith)
+import Partial.Unsafe (unsafePartial)
+
 f :: Partial => Int -> Int
 f 0 = 0
-f _ = Partial.crashWith "f: partial function"
+f _ = crashWith "f: partial function"
 
 safely :: Int
-safely = Partial.Unsafe.unsafePartial (f 0)
+safely = unsafePartial (f 0)
 
 main :: forall a. a -> {}
 main _ = {}
