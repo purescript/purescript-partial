9d5472d516b70ad4bec965a6832d92e3b2710b9a
Harry Garrood <harry@garrood.me>
2017-06-12 18:48:18 +0100

    Deprecate unsafePartialBecause; fixes #7

--- src/Partial/Unsafe.purs
+++ src/Partial/Unsafe.purs
@@ -10,10 +10,9 @@ import Partial (crashWith)
 -- | Discharge a partiality constraint, unsafely.
 foreign import unsafePartial :: forall a. (Partial => a) -> a
 
--- | Discharge a partiality constraint, unsafely. Raises an exception with a
--- | custom error message in the (unexpected) case where `unsafePartial` was
--- | used incorrectly.
-foreign import unsafePartialBecause :: forall a. String -> (Partial => a) -> a
+-- | *deprecated:* use `unsafePartial` instead.
+unsafePartialBecause :: forall a. String -> (Partial => a) -> a
+unsafePartialBecause _ x = unsafePartial x
 
 -- | A function which crashes with the specified error message.
 unsafeCrashWith :: forall a. String -> a
