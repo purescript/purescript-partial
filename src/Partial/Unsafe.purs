39f16e27e5da20c3e772f9e7bc738e86b5cb27fa
Cole Haus <colehaus@cryptolab.net>
2018-05-09 23:13:12 -0700

    Add `showAnyAndCrashWith`
    Motivating use case discussed in more depth in
    https://github.com/garyb/purescript-debug/pull/16

--- src/Partial/Unsafe.purs
+++ src/Partial/Unsafe.purs
@@ -3,12 +3,16 @@ module Partial.Unsafe
   ( unsafePartial
   , unsafePartialBecause
   , unsafeCrashWith
+  , unsafeShowAnyAndCrashWith
   ) where
 
 import Partial (crashWith)
 
 -- | Discharge a partiality constraint, unsafely.
 foreign import unsafePartial :: forall a. (Partial => a) -> a
+
+foreign import showAny :: forall a. a -> String
+foreign import concatString :: String -> String -> String
 
 -- | *deprecated:* use `unsafePartial` instead.
 unsafePartialBecause :: forall a. String -> (Partial => a) -> a
@@ -17,3 +21,9 @@ unsafePartialBecause _ x = unsafePartial x
 -- | A function which crashes with the specified error message.
 unsafeCrashWith :: forall a. String -> a
 unsafeCrashWith msg = unsafePartial (crashWith msg)
+
+-- | A function which crashes with the specified error message and `show`s the
+-- | primitive, underlying representation of the second argument. (This allows
+-- | us to debug without incurring a `Show` constraint.)
+unsafeShowAnyAndCrashWith :: forall a b. String -> a -> b
+unsafeShowAnyAndCrashWith msg a = unsafeCrashWith (concatString msg (showAny a))
